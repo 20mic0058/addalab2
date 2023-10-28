b7b6061202bd579e8e507f3eb4bfdb1885e33b2d
BhargavReddie <130288408+BhargavReddie@users.noreply.github.com>
2023-10-28 20:32:21 +0530

    bhargav-comitted.java

--- src/main/java/mavenproject/UserAuth.java
+++ src/main/java/mavenproject/UserAuth.java
@@ -5,6 +5,7 @@ public class UserAuth {
     private static final String STORED_PASSWORD = "2002";
 
     public static boolean authenticate(String username, String password) {
+	    System.out.println("Bhargav edit");
         return username.equals(STORED_USERNAME) && password.equals(STORED_PASSWORD);
     }
 
